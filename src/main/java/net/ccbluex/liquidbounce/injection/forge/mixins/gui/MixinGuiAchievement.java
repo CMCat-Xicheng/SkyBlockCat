a67a3afa5cf30b194d20f271cf3c39448db85f1a
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-15 21:33:22 +0800

    Update MixinGuiAchievement.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiAchievement.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiAchievement.java
@@ -6,7 +6,6 @@
 package net.ccbluex.liquidbounce.injection.forge.mixins.gui;
 
 import net.ccbluex.liquidbounce.LiquidBounce;
-import net.ccbluex.liquidbounce.features.module.modules.render.NoAchievements;
 import net.minecraft.client.gui.achievement.GuiAchievement;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +16,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinGuiAchievement {
     @Inject(method = "updateAchievementWindow", at = @At("HEAD"), cancellable = true)
     private void injectAchievements(CallbackInfo ci) {
-        if (LiquidBounce.moduleManager != null 
-            && LiquidBounce.moduleManager.getModule(NoAchievements.class) != null 
-            && LiquidBounce.moduleManager.getModule(NoAchievements.class).getState()) 
-            ci.cancel();
-    }
+        
 }
