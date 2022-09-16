2144ce7fe64a7bd2e2128625f59d2a92d6892058
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-16 20:31:17 +0800

    Update NoSlow.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoSlow.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoSlow.kt
@@ -3,6 +3,7 @@ package net.ccbluex.liquidbounce.features.module.modules.movement
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.event.EventState
 import net.ccbluex.liquidbounce.event.MotionEvent
 import net.ccbluex.liquidbounce.event.SlowDownEvent
