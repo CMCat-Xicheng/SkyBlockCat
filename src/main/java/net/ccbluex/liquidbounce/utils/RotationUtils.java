d4b81b9e758bffc209b86cd8c8d3f1e053902c92
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-16 11:29:12 +0800

    Update RotationUtils.java

--- src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.java
@@ -143,7 +143,7 @@ public final class RotationUtils extends MinecraftInstance implements Listenable
         final double posZ = target.posZ + (predict ? (target.posZ - target.prevPosZ) * predictSize : 0) - (player.posZ + (predict ? (player.posZ - player.prevPosZ) : 0));
         final double posSqrt = Math.sqrt(posX * posX + posZ * posZ);
 
-        float velocity = LiquidBounce.moduleManager.getModule(FastBow.class).getState() ? 1F : player.getItemInUseDuration() / 20F;
+        float velocity = player.getItemInUseDuration() / 20F;
         velocity = (velocity * velocity + velocity * 2) / 3;
 
         if(velocity > 1) velocity = 1;
