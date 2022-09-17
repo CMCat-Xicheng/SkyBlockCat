22565e3d72c3868224cb4b82ec707b8ba10aed5e
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-17 10:21:53 +0800

    Create NoSlow.java

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoSlow.java
@@ -0,0 +1,66 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.EventState
+import net.ccbluex.liquidbounce.event.MotionEvent
+import net.ccbluex.liquidbounce.event.SlowDownEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
+import net.ccbluex.liquidbounce.utils.ClientUtils
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.utils.PacketUtils
+import net.ccbluex.liquidbounce.utils.timer.MSTimer
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntegerValue
+import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.item.*
+import net.minecraft.network.Packet
+import net.minecraft.network.play.INetHandlerPlayServer
+import net.minecraft.network.play.client.C0BPacketEntityAction
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition
+import net.minecraft.network.play.client.C03PacketPlayer.C05PacketPlayerLook
+import net.minecraft.network.play.client.C03PacketPlayer.C06PacketPlayerPosLook
+import net.minecraft.network.play.client.C07PacketPlayerDigging
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement
+import net.minecraft.network.play.client.C09PacketHeldItemChange
+import net.minecraft.network.play.server.S30PacketWindowItems
+import net.minecraft.util.BlockPos
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@ModuleInfo(name = "NoSlow", spacedName = "NoSlow", category = ModuleCategory.MOVEMENT, description = "Prevent you from getting slowed down by using abilities")
+class NoSlow : Module() {
+
+    private static boolean isRightClickKeyDown = false;
+
+    @SubscribeEvent
+    public void onTick(TickEndEvent event) {
+        isRightClickKeyDown = mc.gameSettings.keyBindUseItem.isKeyDown();
+    }
+
+    @SubscribeEvent
+    public void onInteract(PlayerInteractEvent event) {
+        if(this -> state && event.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) {
+            if(mc.thePlayer.getHeldItem() != null) {
+                val item = mc.thePlayer.itemInUse.item;
+                if(item is ItemSword) {
+                    event.setCanceled(true);
+                    if(!isRightClickKeyDown) {
+                        PacketUtils.sendPacketNoEvent(new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), 255, Shady.mc.thePlayer.getHeldItem(), 0, 0, 0));
+                    }
+                }
+            }
+        }
+    }
+
+}
