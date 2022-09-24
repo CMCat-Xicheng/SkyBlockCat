0b9bd2e62a295e767119cb01fe021918c5f187b3
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-24 15:44:12 +0800

    Create DungeonScoreCalc.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/DungeonScoreCalc.kt
@@ -0,0 +1,16 @@
+
+package net.ccbluex.liquidbounce.features.module.modules.world;
+
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.utils.ClientUtils;
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+
+@ModuleInfo(name = "DungeonScoreCalc", description = "Calculate your score in dungeon.", category = ModuleCategory.DEVING)
+class ScoreCalc : Module() {
+  
+}
