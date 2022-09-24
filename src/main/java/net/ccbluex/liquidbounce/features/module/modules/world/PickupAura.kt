5e20417595fe76b595e3042687c767b79c4aedc5
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-24 15:40:40 +0800

    Create PickupAura.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/PickupAura.kt
@@ -0,0 +1,16 @@
+
+package net.ccbluex.liquidbounce.features.module.modules.world;
+
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.utils.ClientUtils;
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+
+@ModuleInfo(name = "PickupAura", description = "Allows you to pickup items around you, even through walls.", category = ModuleCategory.DEVING)
+class GhostBlock : Module() {
+  
+}
