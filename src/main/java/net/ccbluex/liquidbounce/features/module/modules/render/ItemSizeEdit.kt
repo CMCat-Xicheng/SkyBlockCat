4acbdcfb8b356924682303c06bca65d20bd47382
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-24 15:41:53 +0800

    Create ItemSizeEdit.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemSizeEdit.kt
@@ -0,0 +1,16 @@
+
+package net.ccbluex.liquidbounce.features.module.modules.world;
+
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.utils.ClientUtils;
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+
+@ModuleInfo(name = "ItemSize", description = "Change the sizes of dropped items.", category = ModuleCategory.DEVING)
+class ItemSize : Module() {
+  
+}
